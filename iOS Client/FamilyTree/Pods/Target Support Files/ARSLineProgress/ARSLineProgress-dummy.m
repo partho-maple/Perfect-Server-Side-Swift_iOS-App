ebad4fc08edcb2f0f6843f88e9da075fa1f50bbc
Partho Biswas <partho.biswas@aurea.com>
2017-05-16 23:15:01 +0600

    Taking down the project for implementation improvement.

--- iOS Client/FamilyTree/Pods/Target Support Files/ARSLineProgress/ARSLineProgress-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ARSLineProgress : NSObject
-@end
-@implementation PodsDummy_ARSLineProgress
-@end
--- iOS Client/FamilyTree/Pods/Target Support Files/Siesta/Siesta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Siesta : NSObject
-@end
-@implementation PodsDummy_Siesta
-@end
